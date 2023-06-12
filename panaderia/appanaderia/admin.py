2ddbc6376aa87b340be1ab84661bfd70651f0cd1
Felipe Del Zoppo <114626945+FelpaZoppo@users.noreply.github.com>
2023-06-12 20:29:23 -0300

    Add files via upload

---
+++ panaderia/appanaderia/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from appanaderia.models import *
+# Register your models here.
+admin.site.register(Cliente)
+admin.site.register(DetalleVenta)
+admin.site.register(Empleado)
+admin.site.register(Localidad)
+admin.site.register(Producto)
+admin.site.register(Venta)
